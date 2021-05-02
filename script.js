55c7cc5484470a6921b36a77a21c4ecabb5b880c
Sleina <55833917+Sleina@users.noreply.github.com>
2021-05-02 11:53:08 +0200

    Display Temperature
    Display actual Temperature of location

--- script.js
+++ script.js
@@ -22,6 +22,30 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
 //Display Weather Forecast
 function displayforecast() {
   let forecastElement = document.querySelector("#forecast");
